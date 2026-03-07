2ed67669cf6fa4738aee927be5ddc7c08b240b21
VSP <k.dodani.k@gmail.com>
2026-03-07 20:17:32 +0100

    minor corrections related to game loop

--- src/core/GameLoop.cpp
+++ src/core/GameLoop.cpp
@@ -2,23 +2,22 @@
 // Created by Krish on 3/7/2026.
 //
 #include "GameLoop.h"
+#include <raylib.h>
 #include <raymath.h>
 
-Camera camera = {.position = Vector3(0, 2.0f, 3.0f),
-  .target = Vector3{0, 0, 0},
+// Camera private to this file, temporary implementation
+static Camera camera = {.position = Vector3(0, 2.0f, 3.0f),
+  .target = Vector3{0, 0.0f, 0},
   .up = {0, 1, 0},
   .fovy = 75.0f,
   .projection = CAMERA_PERSPECTIVE};
 
-void GameLoop::Update(Player& player, Camera& camera) {
+
+void GameLoop::Update(Player& player) {
   float dt = GetFrameTime();
   player.Update(dt);
   camera.position = player.position + Vector3{0, 1.8f, 0.0f}; // Raise to eye-level
-  camera.target = player.position + Vector3{0, 0.0f, -1.0f}; // Look to front*/
-}
-
-float GameLoop::GetDeltaTime() {
-  return GetFrameTime();
+  camera.target = player.position + Vector3{0, 1.8f, -1.0f}; // Look to front*/
 }
 
 void GameLoop::Draw(const Player& player) {
@@ -27,18 +26,17 @@ void GameLoop::Draw(const Player& player) {
   BeginMode3D(camera);
   DrawPlane(
     Vector3{0.0f, 0.0f, 0.0f}, // Ground center coords
-    Vector2{5, 5}, // Size of plane
+    Vector2{15, 15}, // Size of plane
     DARKGRAY);
 
   DrawCapsule(
     player.position,
-    player.position + Vector3{-1.0f, -1.0f, -2.0f},
+    player.position + Vector3{0, 1.8f, 0},
     0.5f,
     8,
     8,
     PURPLE
     );
-
   EndMode3D();
   EndDrawing();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,14 @@
 #include <raylib.h>
 #include "core/GameLoop.h"
 
-extern Camera camera;
-
 int main() {
   InitWindow(1280, 720, "attempt_at_game");
   SetTargetFPS(60);
   Player player;
-  player.position = Vector3(0,1.5f, -2.0f);
+  player.position = Vector3(0,0.25f, -2.0f);
   player.velocity = Vector3(0,0,0);
   while (!WindowShouldClose()) {
-    GameLoop::Update(player, camera);
+    GameLoop::Update(player);
     GameLoop::Draw(player);
   }
   CloseWindow();
